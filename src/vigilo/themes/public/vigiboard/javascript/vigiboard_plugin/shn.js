8acec6fc232525155eac104b9559a2ddb9a335d1
Francois POIROTTE <francois.poirotte@c-s.fr>
2009-12-09 17:59:21 +0000

    Déplacement de JxLib dans les fichiers communs. Mise à jour des traductions. Mise à jour des templates de Vigiboard suite au passage à mootools. Légère amélioration des images pour les flèches précédent/suivant dans VigiBoard.
    Ajout d'une feuille de style pour Rum dans VigiCore.
    
    
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1409 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- src/vigilo/themes/public/vigiboard/javascript/vigiboard_plugin/shn.js
+++ src/vigilo/themes/public/vigiboard/javascript/vigiboard_plugin/shn.js
@@ -1,15 +1,14 @@
-
-function vigiboard_shndialog(url,idd) {
-	$.getJSON(url+'get_plugin_value',{plugin_name:'shn',idcorrevent:idd},function(json){ 				
-		var ht = '';
-		for (var i = 0; i < json.shns.length; i++) {
-			ht += '<li>' + json.shns[i] + '</li>';
-		}
-		$('#SHNDialog_liste').html(ht);
-		$('#SHNDialog').dialog('open');
-	})
+function vigiboard_hls_dialog(ref, url, idd) {
+    var req = new Request.JSON({
+        url: url + 'get_plugin_value',
+        onSuccess: function (json, raw) {
+            var code = '';
+            for (var i = 0; i < json.services.length; i++)
+                code += '<p>' + json.services[i] + '</p>';
+            window.hls_dialog.setContent(code);
+            window.hls_dialog.open.bind(window.hls_dialog)();
+        }
+    });
+    req.post({plugin_name: 'shn', idcorrevent: idd});
 }
-$('.SHNLien').each(function() {
-	$(this).click(function(e){
-	$('#SHNDialog').dialog('option','position',[e.clientX+10,e.clientY]);
-	})});
+
