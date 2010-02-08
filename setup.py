80a2eedf7057255bd72b29fa8bcd305910e94b4f
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-02-08 13:40:15 +0000

    Remplacement des fichiers de configuration Python (settings.py) en fichiers INI (settings.ini), resp. pour les settings de test. Correction des dépendances entre les composants (vigiboard -> vigilo.turbogears -> vigilo.models) et sur les bibliothèques. Réintégration de la configuration de SQLAlchemy dans les fichiers INI de Turbogears. Mise à jour de la configuration du corrélateur à titre d'exemple. La manière de configurer les loggers a RADICALEMENT changée !! Lisez les settings du corrélateur pour un exemple.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1866 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -2,56 +2,60 @@
 # vim: set fileencoding=utf-8 sw=4 ts=4 et :
 from setuptools import setup, find_packages
 
-setup(name='vigilo-themes-default',
-        version='0.1',
-        author='Francois POIROTTE',
-        author_email='francois.poirotte@c-s.fr',
-        url='http://www.projet-vigilo.org/',
-        description='default theme for vigilo applications',
-        license='http://www.gnu.org/licenses/gpl-2.0.html',
-        long_description='This component provides a default theme for vigilo applications:\n',
-        zip_safe=False,
-        install_requires=[
-            'setuptools',
-            'coverage',
-            'nose',
-            'pylint',
-            'Genshi >= 0.5.1',
+tests_require = [
+    'coverage',
+    'nose',
+    'pylint',
+]
 
-            'Babel >= 0.9.4',
+setup(name='vigilo-themes-default',
+    version='0.1',
+    author='Francois POIROTTE',
+    author_email='francois.poirotte@c-s.fr',
+    url='http://www.projet-vigilo.org/',
+    description='default theme for vigilo applications',
+    license='http://www.gnu.org/licenses/gpl-2.0.html',
+    long_description='This component provides a default theme for Vigilo',
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Genshi >= 0.5.1',
+        'Babel >= 0.9.4',
+    ],
+    namespace_packages = [
+        'vigilo',
+    ],
+    packages=find_packages('src', exclude=['ez_setup']),
+    include_package_data = True,
+    test_suite='nose.collector',
+    tests_require=tests_require,
+    extras_require={
+        'tests': tests_require,
+    },
+    package_data={'vigilo.themes': [
+        'i18n/*/LC_MESSAGES/**.*',
+        'templates/*/**.*',
+        'templates/*/**.html',
+        'templates/*/*/**.html',
+        'public/*/css/**.css',
+        'public/*/css/*/**.*',
+        'public/*/css/*/*/**.*',
+        'public/*/javascript/**.js',
+        'public/*/javascript/*/**.js',
+        'public/*/images/**.*',
+    ]},
+    exclude_package_data ={'vigilo.themes': [
+    ]},
+    message_extractors={'src/vigilo/themes/': [
+        ('**/templates/**.mako', 'mako', None),
+        ('**/templates/**.html', 'genshi', None),
+        ('**/templates/admin/**.html', 'genshi', None),
+        ('**/public/**', 'ignore', None)
+    ]},
+    entry_points={
+        'console_scripts': [
             ],
-        namespace_packages = [
-            'vigilo',
-            ],
-        packages=[
-            'vigilo',
-            'vigilo.themes',
-        ],#find_packages('src'),
-        include_package_data = True,
-        package_data={'vigilo.themes': [
-            'i18n/*/LC_MESSAGES/**.*',
-            'templates/*/**.*',
-            'templates/*/**.html',
-            'templates/*/*/**.html',
-            'public/*/css/**.css',
-            'public/*/css/*/**.*',
-            'public/*/css/*/*/**.*',
-            'public/*/javascript/**.js',
-            'public/*/javascript/*/**.js',
-            'public/*/images/**.*',
-        ]},
-        exclude_package_data ={'vigilo.themes': [
-        ]},
-        message_extractors={'src/vigilo/themes/': [
-            ('**/templates/**.mako', 'mako', None),
-            ('**/templates/**.html', 'genshi', None),
-            ('**/templates/admin/**.html', 'genshi', None),
-            ('**/public/**', 'ignore', None)
-        ]},
-        entry_points={
-            'console_scripts': [
-                ],
-            },
-        package_dir={'': 'src'},
-        )
+        },
+    package_dir={'': 'src'},
+)
 
