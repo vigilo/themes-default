69acffd3af80048f340f57e4eb3dbde2d518f48f
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-04-02 13:41:15 +0000

    Passage en version 2.0.0
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@2585 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='0.1',
+    version='2.0.0',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
