f25177ad3ca2f4667d3ba7dac86b321bd1bc51ef
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-11-17 09:12:27 +0000

    Fusion du nouveau VigiGraph basé sur mootools dans le trunk.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@5124 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/themes/public/vigigraph/js/search.js
@@ -0,0 +1,104 @@
+var SearchResultsModel = new Class({
+    Extends: Jx.Grid.Model,
+
+    getColumnCount: function () {
+        return (this.data && this.data.labels && this.data.labels[0]) ?
+            this.data.labels[0].length : 0;
+    },
+
+    getRowCount: function () {
+        return (this.data && this.data.labels) ? this.data.labels.length : 0;
+    },
+
+    getValueAt: function (row, col) {
+        return (this.data && this.data.labels && $chk(this.data.labels[row])) ?
+            this.data.labels[row][col] : '';
+    },
+
+    getIDAt: function (row, col) {
+        return (this.data && this.data.ids && $chk(this.data.ids[row])) ?
+            this.data.ids[row][col] : null;
+    }
+});
+
+window.addEvent('load', function () {
+    function update_search_results(data) {
+        // On met à jour le contenu de la grille
+        // avec les résultats de la recherche.
+        var model = new SearchResultsModel(data, {
+            colWidth: 255,
+            columnHeaders: [_('Host'), _('Graph')]
+        });
+
+        function selectCell(row, col) {
+            var host = this.getValueAt(row, 0);
+            var graph = this.getValueAt(row, 1);
+            var idhost = this.getIDAt(row, 0);
+            var idgraph = this.getIDAt(row, 1);
+
+            // Sélection d'un graphe.
+            if (col == 1) {
+                // La recherche ne concernait que l'hôte.
+                if (graph == '')
+                    return;
+                window.toolbar.host_picker.setItem(idhost, host);
+                window.toolbar.graph_picker.setItem(idgraph, graph);
+                window.toolbar.show_graph.clicked(null);
+            }
+            // Sélection d'un hôte.
+            else {
+                window.toolbar.host_picker.setItem(idhost, host);
+                // On restaure le sélecteur de graphe dans son état initial,
+                // où il affiche un message "Sélectionnez un graphe".
+                window.toolbar.graph_picker.setItem(null,
+                    window.toolbar.graph_picker.options.label);
+                window.toolbar.show_nagios.clicked(null);
+            }
+        }
+
+        model.addEvent('select-cell', selectCell.bind(model));
+        search_results.setModel(model);
+    }
+
+    search_results = new Jx.Grid({
+        alternateRowColors: true,
+        columnHeaders: true,
+        rowPrelight: true,
+        cellSelection: true
+    }).addTo('search_results');
+
+    search_dialog = new Jx.Dialog({
+        label: _('Search for an host/graph'),
+        modal: false,
+        width: 600,
+        height: 400,
+        content: 'search_container'
+    });
+
+    search_request = new Request.JSON({
+        url: app_path + 'rpc/searchHostAndGraph',
+        method: 'post',
+        onSuccess: update_search_results
+    });
+
+    $('search').addEvent('click', function (e) {
+        e.preventDefault();
+        search_dialog.open();
+    });
+
+    search_dialog.addEvent('open', function () {
+        // On doit afficher une grille vide la première fois,
+        // afin de donner les bonnes dimensions à la grille.
+        if (search_results.model)
+            return;
+        search_results.setModel(new Jx.Grid.Model([], {
+            colWidth: 255,
+            columnHeaders: [_('Host'), _('Graph')]
+        }));
+    });
+
+    $('search_form_search').addEvent('click', function (e) {
+        e.preventDefault();
+        search_request.send($('search_form').toQueryString());
+    });
+});
