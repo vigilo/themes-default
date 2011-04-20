e0075244163989ac83070b5e4a32d645d406f831
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-04-20 10:48:58 +0000

    Factorisation de l'utilisation des bibliothèques JS
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@6851 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ src/vigilo/themes/public/common/js/Autocompleter.Request.VigiloJSON.js
@@ -0,0 +1,27 @@
+/**
+ * Autocompleter.Request.VigiloJSON
+ *
+ */
+
+Autocompleter.Request.VigiloJSON = new Class({
+    Extends: Autocompleter.Request.JSON,
+
+    queryResponse: function(response) {
+        this.parent();
+        this.update(response.results);
+    },
+
+    // Correction d'un bug qui vide le champ si on clique trop vite dessus puis en dehors
+    // http://digitarald.de/forums/topic.php?id=506&replies=1#post-1319
+    hideChoices: function(clear) {
+        var fixed = false;
+        if (clear && !this.opted) {
+            fixed = true;
+            this.opted = this.element.value;
+        }
+        this.parent(clear);
+        if (fixed) { // revert the fix
+            this.opted = undefined;
+        }
+    }
+});
