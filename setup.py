81f49634ed36e4c927e5fadd821fcd8bd762e67e
Francois POIROTTE <francois.poirotte@c-s.fr>
2011-08-05 19:19:28 +0200

    Bump des n° de version (partie commune)
    Change-Id: Ifcd860ed952b4ede770b262688d212bb3ee51a28
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/142
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='2.0.3',
+    version='2.0.4',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
