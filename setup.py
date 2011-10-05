a051d2bcd6a8e88703a1134b00a4cb8ea7debffd
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-10-05 17:08:44 +0200

    MAJ versions
    BuildBot: no-build
    Change-Id: If0df1d6d6e73a44540f63c5454270543d64aebad

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='2.0.6',
+    version='2.0.7',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
