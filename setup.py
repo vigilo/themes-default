1d40f82647d445c197b1bdfe452f3e887786d95e
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-12-08 15:26:08 +0100

    Mise à jour des versions
    Utilisation de la commande :
    `vigilo-dev-update-versions -d . -s .../r2.17`
    
    BuildBot: no-build
    Change-Id: I4f494ffba573f613a3076472f7bc3014cdfede79

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='2.0.7',
+    version='2.0.8',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
