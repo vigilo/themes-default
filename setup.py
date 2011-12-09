02196fbcf0247c416a1b443aaf744c1d9fe115ea
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-12-09 10:46:52 +0100

    Nouvelle mise à jour des versions, complète cette fois
    Pour éviter un bug dans la détection des modules modifiés, on met à jour
    les versions de tous les modules.
    
    BuildBot: no-build
    Change-Id: Ia873846472018918f1770beb57271b528cfc27e6

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='2.0.8',
+    version='2.0.9',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
