cbd82b321f1bed70fd7b83e714a05e581729770b
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-02-07 17:20:15 +0100

    Bump des versions (suite livraison un client 2.19)
    Change-Id: Ibc6459639546ce2a273f02c3c7a8b8a912122b80
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='2.0.13',
+    version='2.0.14',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
