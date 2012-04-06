b826d87e8d4481249c47e1fc59d3c75758f76340
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-04-06 11:42:48 +0200

    Ajout et mises à jour de copyrights, complète le [sha:3c99844]
    Change-Id: I47c53310f710006097927c666387207c1f4125aa
    Fixup: 3c99844c7630b66e3f267cc08d169a5c91b73572
    BuildBot: no-build

--- src/vigilo/themes/public/vigigraph/js/print.js
+++ src/vigilo/themes/public/vigigraph/js/print.js
@@ -1,3 +1,6 @@
+// Copyright (C) 2011-2012 CS-SI
+// License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
 window.addEvent('domready', function () {
     $('print').addEvent('click', function (e) {
         e.preventDefault();
