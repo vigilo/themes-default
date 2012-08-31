e279b3ee507602d6c7d3588ade01c32651ebed82
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2012-08-31 12:27:11 +0200

    Bump des versions (suite livraison r5.3)
    Change-Id: Ib57df24eef163008857717f22034053902349348
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/843
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='2.1.1',
+    version='2.1.2',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
