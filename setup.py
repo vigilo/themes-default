c9629f3f858e84cc700574db8fcf87eb9444a13f
Francois POIROTTE <francois.poirotte@c-s.fr>
2014-05-12 14:30:48 +0200

    Bump des versions
    Change-Id: I16667f5212c272c4185f14412f1fa67d6831f04c
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1412
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='3.4',
+    version='3.5',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
