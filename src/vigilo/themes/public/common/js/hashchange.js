5e991f8152499f6fc87d7e7765812ec5a5b5e6c4
Francois POIROTTE <francois.poirotte@c-s.fr>
2014-11-04 15:59:26 +0100

    [#1266] Historique navigateur et fragment.
    Modifie le fonctionnement du chargement des cartes/graphes afin de se
    baser sur les changements du fragment dans l'URL, tout en conservant les
    fonctionnalités d'historique du navigateur. Ceci afin qu'un clic sur le
    bouton Précédent/Suivant mette bien à jour l'état de l'application et
    son contenu in fine.
    
    Ce changement introduit le fichier "hashchange.js" qui permet d'ajouter
    le support de l'événement "onhashchange" sur "document" pour tous les
    navigateurs.
    
    Pour le moment, seul !VigiMap est concerné, mais le même fonctionnement
    sera mis en place pour !VigiGraph ultérieurement.
    
    Change-Id: Ia3761524629e0d9697365f0bcfcc691b6be6af43
    Refs: #1266.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1690
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: POIROTTE <francois.poirotte@c-s.fr>

---
+++ src/vigilo/themes/public/common/js/hashchange.js
@@ -0,0 +1,24 @@
+/* Repris de https://mootools.lighthouseapp.com/projects/2706/tickets/778-add-hashchange-event */
+
+Element.Events.hashchange = {
+    onAdd: function(){
+        var hash = null;
+
+        var hashchange = function(){
+            if (hash === self.location.hash) return;
+            else hash = self.location.hash;
+
+            var value = (hash.indexOf('#') === 0 ? hash.substr(1) : hash);
+            window.fireEvent('hashchange', value);
+            document.fireEvent('hashchange', value);
+        };
+
+        if ("onhashchange" in window){
+            window.onhashchange = hashchange;
+        } else {
+            // Détection des changements tous les 100ms.
+            this.periodical(100);
+        }
+        hashchange();
+    }
+};
