be142da31c61ed87fe708317391c234952b59d20
Francois POIROTTE <francois.poirotte@c-s.fr>
2015-05-18 16:12:18 +0200

    Bump version 3.5.1
    Change-Id: I0b124f31939858f38ec4a183707cb8292287f846
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1766
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='3.5',
+    version='3.5.1',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     url='http://www.projet-vigilo.org/',
