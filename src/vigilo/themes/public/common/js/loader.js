a6ad758777de4ac5cb0ceaadaae7cb0e95c7e5f3
Francois POIROTTE <francois.poirotte@c-s.fr>
2015-11-12 16:00:56 +0100

    [#1457] Ajout fichier oublié.
    Le fichier loader.js avait été oublié dans les précédents commits,
    en lien avec le chargement AJAX dans VigiBoard.
    
    Change-Id: Ifd068c8d0a13fd4ca72c33371916565d1cc77b19
    Refs: #1457.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1921
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

---
+++ src/vigilo/themes/public/common/js/loader.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2009-2015 CS-SI
+ * Licence : GNU GPL v2 ou superieure
+ *
+ */
+/*
+ * Icone de chargement
+ */
+var Loader = new Class({
+
+    clients: 0, // Nombre de demandes d'affichage
+
+    initialize: function() {
+        this.element = null;
+        this.layer = null;
+        window.addEvent('domready', function () {
+            this.element = $("loading");
+            this.layer = $("loadingLayer");
+        }.bind(this));
+        this.logger = (new Log()).enableLog();
+    },
+
+    show: function() {
+        this.clients += 1;
+        if (!$chk(this.element) || this.element.getStyle("display") == "block") {
+            return this;
+        }
+        this.logger.log("Showing spinner");
+        this.element.setStyle("display", "block");
+        if (this.layer) {
+            this.layer.setStyle("opacity", "0.5");
+        }
+        return this;
+    },
+
+    hide: function() {
+        this.clients -= 1;
+        // Encore des demandes d'affichage en attente
+        if (!$chk(this.element) || this.clients > 0) return this;
+        if (this.element.getStyle("display") == "none") return this;
+        this.logger.log("Hiding spinner");
+        this.element.setStyle("display", "none");
+        if (this.layer) {
+            this.layer.setStyle("opacity", "auto");
+        }
+        return this;
+    }
+});
+
+loader = new Loader();
+
+// Affichage du loader pour chaque requete
+window.Request = Class.refactor(Request, {
+    initialize: function(options){
+        this.previous(options);
+        this.addEvent("request", loader.show.bind(loader));
+        ['complete', 'exception', 'cancel'].each(function(event){
+            this.addEvent(event, loader.hide.bind(loader));
+        }, this);
+    }
+});
