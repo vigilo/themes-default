dfd488d48b3a1a1f6793919455118c93fa874d54
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-06-08 16:55:35 +0200

    Génération/parsing d'URL au format TurboGears
    Patche le code de mootools/mootools-more pour que les URLs générées le
    soient au format de TurboGears plutôt qu'au format PHP.
    
    NB : le schéma d'encodage choisi par TurboGears ne permet pas de
         distinguer une valeur scalaire d'un tableau contenant une seule
         valeur. Le code de mootools décodera ce cas particulier comme une
         valeur scalaire. L'application devra convertir la valeur en tableau
         si nécessaire (cf. fonction $splat de mootools).
    
    Change-Id: I38819fd7032c59b3559fbcd587324248572e846a
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/2412
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>

---
+++ src/vigilo/themes/public/common/js/mootools-vigilo.js
@@ -0,0 +1,67 @@
+/* Par défaut, mootools encore/décode les paramètres d'URL (query strings)
+ * en utilisant les conventions de PHP (ie. "a[b]=42&b[]=23&b[]=69").
+ * On patche les fonctions liées à ces opérations pour utiliser les conventions
+ * de TurboGears à la place (ie. "a.b=42&b=23&b=69").
+ *
+ * Note :
+ * La notation de TurboGears ne permet pas de différencier une valeur scalaire
+ * d'un tableau contenant une seule valeur.
+ * L'application appelante est chargée de réaliser les conversions nécessaires
+ * en fonction du contexte.
+ */
+
+String.implement({
+	parseQueryString: function(decodeKeys, decodeValues){
+		if (decodeKeys == null) decodeKeys = true;
+		if (decodeValues == null) decodeValues = true;
+		var vars = this.split(/[&;]/), res = {};
+		if (vars.length) vars.each(function(val){
+			var index = val.indexOf('='),
+				keys = index < 0 ? [''] : val.substr(0, index).match(/([^.]+)/g),
+				value = decodeValues ? decodeURIComponent(val.substr(index + 1)) : val.substr(index + 1),
+				obj = res;
+			keys.each(function(key, i){
+				if (decodeKeys) key = decodeURIComponent(key);
+				var current = obj[key];
+				if(i < keys.length - 1)
+					obj = obj[key] = current || {};
+				else if($type(current) == 'array')
+					current.push(value);
+				else
+					obj[key] = $defined(current) ? [current, value] : value;
+			});
+		});
+		return res;
+	}
+});
+
+Hash.implement({
+	toQueryString: function(base){
+		var queryString = [];
+		Hash.each(this, function(value, key){
+			if (base) key = base + '_' + key;
+			var result;
+			switch ($type(value)){
+				case 'object': result = Hash.toQueryString(value, key); break;
+				case 'array':
+					value.each(function(val, i){
+						switch ($type(val)) {
+							case 'object':
+							case 'array':
+								queryString.push(Hash.toQueryString(val, key));
+								break;
+							default:
+								queryString.push(key + '=' + encodeURIComponent(val));
+								break;
+						}
+					});
+					value = undefined;
+				break;
+				default: result = key + '=' + encodeURIComponent(value);
+			}
+			if (value != undefined) queryString.push(result);
+		});
+
+		return queryString.join('&');
+	}
+});
