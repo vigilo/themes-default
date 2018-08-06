89fed407af993b9638c1c8b7d4c6bad63d4ebd31
Francois POIROTTE <francois.poirotte@c-s.fr>
2018-08-06 17:59:59 +0200

    Préparation Vigilo v5.0.0
    Change-Id: Iea197c4fc4475145d47d290b98a7253fa58f2cfb

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ tests_require = [
 ]
 
 setup(name='vigilo-themes-default',
-    version='4.1.0',
+    version='5.0.0',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     url='https://www.vigilo-nms.com/',
